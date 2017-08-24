0346adeb5f366d2b2654e6f92703680c0a793e32
netmonarch <30328721+netmonarch@users.noreply.github.com>
2017-08-24 16:48:22 -0700

    Add files via upload

---
+++ PlayerClass.h
@@ -0,0 +1,43 @@
+#pragma once
+
+// Player Class for Project, "Don't Be Late"
+
+// ************ Item Key ***********
+/*
+	k = car keys		
+	c = coffee beans	
+	s = clothes	
+	p = phone
+	w = wallet
+*/
+// *********************************
+
+
+// ************ Location Key *******
+/*
+	1 - Bedroom		 2 - Bathroom		 3 - Hallway
+	4 - Stairs		 5 - Living Area	 6 - Weight Room
+	7 - Kitchen		 8 - D. Bathroom	 9 - Garage
+*/
+// *********************************
+
+
+
+// Player Class and Construct:
+class Player
+{
+public:
+	string PlayerName; // What do you want the player to be called?
+	char PlayerGender; // Gender of the player; "M" for male, "F" for female
+	int PlayerLocation; // Where is the player in the house? (See Location key)
+	char Inventory[10]; // See item key
+};
+
+Player(string Name, char Gender)
+{
+	PlayerName = Name;
+	PlayerGender = Gender;
+	PlayerLocation = 1;
+	Inventory = {"", "", "", "", "", "", "", "", "", ""}
+}
+// End Player Class and Construct
